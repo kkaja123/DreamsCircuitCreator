8296c43cd6c3c606a6565de627b11494adc7bdb2
Kaja, Kyle <kylekaja123@gmail.com>
2019-04-27 22:44:58 -0500

    Add prototypical features
    * New top bar!
    * Prototype calculator image added to canvas
    * Click and drag of calc image!
    * Sizing of canvas isn't ideal (pls CSS...)

--- circuitcreator.js
+++ circuitcreator.js
@@ -6,21 +6,130 @@
  */
 
 const canvas = $("#graphics");
-const context = canvas[0].getContext("2d");;
+const canvasContainer = $(".canvas-container");
+const menuBar = $(".top-bar");
+const context = canvas[0].getContext("2d");
+var items = [];
+var isMouseDown = false;
+
+var calc = {
+  img: new Image(),
+  x: 100,
+  y: 100,
+  offsetX: 0,
+  offsetY: 0,
+  width: 0,
+  height: 0,
+  isHeld: false,
+  handleImageLoad: function (){
+    drawImageOnCanvas(this.img, this.x, this.y);
+  }
+}
 
 function refreshCanvas()
 {
-
+  context.clearRect(0, 0, canvas.width(), canvas.height());
+  for (let item of items)
+  {
+    drawImageOnCanvas(item.img, item.x, item.y);
+  }
 }
 
+/**********************************************************
+ * Image Functions
+ *********************************************************/
+function drawImageOnCanvas(imgHandle, x, y)
+{
+  context.drawImage(imgHandle, x, y);
+}
+
+/**********************************************************
+ * Canvas Handlers
+ *********************************************************/
+function canvasClickBegin(e)
+{
+  x = e.offsetX;
+  y = e.offsetY;
+
+  for (let item of items)
+  {
+    if (x > item.x &&
+        x < item.x + item.width &&
+        y > item.y &&
+        y < item.y + item.height)
+    {
+      console.log("Detected a click on item.");
+      item.isHeld = true;
+
+      item.offsetX = x - item.x;
+      item.offsetY = y - item.y;
+    }
+  }
+
+  isMouseDown = true;
+}
+
+function canvasMouseMove(e)
+{
+  if (isMouseDown)
+  {
+    x = e.offsetX;
+    y = e.offsetY;
+
+    for (let item of items)
+    {
+      if (item.isHeld)
+      {
+        item.x = x - item.offsetX;
+        item.y = y - item.offsetY;
+
+        if (item.x < 0)
+        {
+          item.x = 0;
+        }
+        if (item.y < 0)
+        {
+          item.y = 0;
+        }
+      }
+    }
+
+    refreshCanvas();
+  }
+}
+
+function canvasClickEnd(e)
+{
+  for (let item of items)
+  {
+    if (item.isHeld)
+    {
+      item.isHeld = false;
+    }
+  }
+
+  isMouseDown = false;
+}
+
+/**********************************************************
+ * Initialization
+ *********************************************************/
 function init()
 {
   canvas.attr({
     width: window.innerWidth,
     height: window.innerHeight,
   });
+  calc.img.onload = $.proxy(calc.handleImageLoad, calc);
+  calc.img.src = "assets/icons/Calculator_prototype.png";
+  calc.width = calc.img.naturalWidth;
+  calc.height = calc.img.naturalHeight;
 
+  canvas.mousedown(canvasClickBegin);
+  canvas.mousemove(canvasMouseMove);
+  canvas.mouseup(canvasClickEnd);
 
+  items.push(calc);
 }
 
 $(init);
