a3d1596683eb34ae6777b5aaba2829cbfb2c46e7
Kaja, Kyle <kylekaja123@gmail.com>
2019-04-29 20:57:11 -0500

    Add many initial features for gadgets, pins, and wires
    * Some features are DEV MODE only, REMOVE THESE FEATURES BEFORE RELEASE!
    * Image scaling is now done through canvas.drawImage.
    * Pin positioning has been corrected.
    * Pins are now drawn immediately.
    * Pins will move when the owning gadget is moved.
    * Gadgets now take their pins' extents when doing canvas edge collision.
    * Movement mouse input is now a document event instead of canvas. Allows for moving items when
    the mouse leaves the canvas (i.e. header, palette, and everywhere else!).
    * Added Wire class with bezier curve support. Bezier strength adjusts dynamically as the wire changes position!
    * Added wire array to hold all wire objects for drawing and manipulation.
    * Added logic to make sure only one item can be held at a time.
    * Removed many debug console.log messages.
    * Gadgets now do their own bounds collision detecting and x/y manipulation.
    * Added a second gadget on init for DEV MODE.

--- circuitcreator.js
+++ circuitcreator.js
@@ -10,7 +10,9 @@ const canvasContainer = $(".canvas-container");
 const menuBar = $(".top-bar");
 const context = canvas[0].getContext("2d");
 var gadgets = [];
+var wires = [];
 var isMouseDown = false;
+var isItemHeld = false;  // Used to make sure to only grab a single item
 var calc;
 
 /**********************************************************
@@ -19,55 +21,6 @@ var calc;
 function isString(s)
 {
   return typeof(s) === 'string' || s instanceof String;
-}
-
-/**********************************************************
- * Pin class definition
- *********************************************************/
-class Pin
-{
-  constructor(pinType, isEmpty)
-  {
-    var type = pinType.toLowerCase();
-
-    if (type != "in" && type != "out")
-    {
-      throw "Pin type " + type + "is not a valid pin type. Use 'in' or 'out'.";
-    }
-
-    this.pinType = type;
-    this.imgDim = 10;  // Image will be scaled to this dimension in both directions.
-    this.img = new Image();
-    this.img.onload = $.proxy(this.handleImageLoad, this);
-    if (!isEmpty && type === "in")
-    {
-      this.img.src = "assets/icons/input_pin.png";
-    }
-    else if (!isEmpty && type === "out")
-    {
-      this.img.src = "assets/icons/output_pin.png";
-    }
-
-    // Declare fields
-    this.x = 0;
-    this.y = 0;
-    this.connectedTo;  // Object that this Pin is connected to. Usually another Pin or an EmptyPoint.
-    this.isEmpty = isEmpty;  // Set to true if this Pin is not attached to a gadget.
-  }
-
-  handleImageLoad()
-  {
-    // TODO: Resize image to imgDim size.
-  }
-
-  getBounds()
-  {
-    return { x0 : this.x,
-             x1 : this.x + this.imgDim,
-             y0 : this.y,
-             y1 : this.y + this.imgDim,
-           };
-  }
 }
 
 /**********************************************************
@@ -96,26 +49,26 @@ class Gadget
     }
 
     // Declare fields
-
     this.baseImg = new Image();
+    this.baseImg.onload = $.proxy(this.handleImageLoad, this);
     this.symbolImg = new Image();
+    this.symbolImg.onload = $.proxy(this.handleImageLoad, this);
     this.x = 0;
     this.y = 0;
     this.offsetX = 0;
     this.offsetY = 0;
-    this.width = 0;
-    this.height = 0;
+    this.width = 80;
+    this.height = 80;
     this.isHeld = false;
-    this.handleImageLoad = function () {
-      calc.width = calc.baseImg.naturalWidth;
-      calc.height = calc.baseImg.naturalHeight;
-
-      drawImageOnCanvas(this.baseImg, this.x, this.y);
-    }
     this.pins = {
       input : [],
       output : [],
     }
+  }
+
+  handleImageLoad()
+  {
+    refreshCanvas();
   }
 
   getPinArray(pinType)
@@ -178,7 +131,7 @@ class Gadget
       }
       else if (pinType === "out")
       {
-        pin.x = this.x + pin.imgDim;
+        pin.x = this.x + this.width;
       }
       else
       {
@@ -192,9 +145,10 @@ class Gadget
   drawGadgetOnCanvas()
   {
     // Draw base image
-    drawImageOnCanvas(this.baseImg, this.x, this.y);
+    drawImageOnCanvas(this.baseImg, this.x, this.y, this.width, this.height);
     // Draw symbol image
     // TODO: Draw symbol overlay
+    this.repositionPins();
     this.drawPinsOnCanvas();
   }
 
@@ -210,17 +164,136 @@ class Gadget
 
     for (let pin of pinArray)
     {
-      drawImageOnCanvas(pin.img, pin.x, pin.y);
+      drawImageOnCanvas(pin.img, pin.x, pin.y, pin.imgDim, pin.imgDim);
     }
+  }
+
+  doBoundsCheck(width, height)
+  {
+    // Left bounds
+    var leftModifier = 0;
+    if (this.pins.input.length > 0)
+    {
+      leftModifier = this.pins.input[0].imgDim;
+    }
+    if (this.x - leftModifier < 0)
+    {
+      this.x = 0 + leftModifier;
+    }
+    // Right bounds
+    var rightModifier = 0;
+    if (this.pins.output.length > 0)
+    {
+      rightModifier = this.pins.output[0].imgDim;
+    }
+    if (this.x + this.width + rightModifier > width)
+    {
+      this.x = width - this.width - rightModifier;
+    }
+    // Top Bounds
+    if (this.y < 0)
+    {
+      this.y = 0;
+    }
+    // Bottom bounds
+    if (this.y + this.height > height)
+    {
+      this.y = height - this.height;
+    }
+  }
+}
+
+/**********************************************************
+ * Pin class definition
+ *********************************************************/
+class Pin
+{
+  constructor(pinType, isEmpty)
+  {
+    var type = pinType.toLowerCase();
+
+    if (type != "in" && type != "out")
+    {
+      throw "Pin type " + type + "is not a valid pin type. Use 'in' or 'out'.";
+    }
+
+    this.pinType = type;
+    this.imgDim = 15;  // Image will be scaled to this dimension in both directions.
+    this.img = new Image();
+    this.img.onload = $.proxy(this.handleImageLoad, this);
+    if (!isEmpty && type === "in")
+    {
+      this.img.src = "assets/icons/input_pin.png";
+    }
+    else if (!isEmpty && type === "out")
+    {
+      this.img.src = "assets/icons/output_pin.png";
+    }
+
+    // Declare fields
+    this.x = 0;
+    this.y = 0;
+    this.connectedTo;  // Object that this Pin is connected to. Usually another Pin or an EmptyPoint.
+    this.isEmpty = isEmpty;  // Set to true if this Pin is not attached to a gadget.
+  }
+
+  handleImageLoad()
+  {
+    refreshCanvas();
+  }
+
+  getBounds()
+  {
+    return { x0 : this.x,
+             x1 : this.x + this.imgDim,
+             y0 : this.y,
+             y1 : this.y + this.imgDim,
+           };
+  }
+}
+
+/**********************************************************
+ * Wire class definition
+ *********************************************************/
+class Wire
+{
+  constructor(inX, inY, outX, outY)
+  {
+    this.input = {x: inX, y: inY};
+    this.output = {x: outX, y: outY};
+
+    // Declare fields
+    this.lineCap = "round";
+    this.color = "#00b33c";
+    this.lineWidth = 4;
+    this.bezierControlPointStrength = 50;
+  }
+
+  drawWire()
+  {
+    context.lineCap = this.lineCap;
+    context.strokeStyle = this.color;
+    context.lineWidth = this.lineWidth;
+
+    context.beginPath();
+    context.moveTo(this.input.x, this.input.y);
+    var bezierControlPointAmount = Math.sqrt(Math.pow(this.output.x - this.input.x, 2) + Math.pow(this.output.y - this.input.y, 2)) / 100 * this.bezierControlPointStrength;
+    context.bezierCurveTo(this.input.x + bezierControlPointAmount,
+                          this.input.y,
+                          this.output.x - bezierControlPointAmount,
+                          this.output.y,
+                          this.output.x,
+                          this.output.y);
+    context.stroke();
   }
 }
 
 /**********************************************************
  * Image Functions
  *********************************************************/
-function drawImageOnCanvas(imgHandle, x, y)
+function drawImageOnCanvas(imgHandle, x, y, width, height)
 {
-  context.drawImage(imgHandle, x, y);
+  context.drawImage(imgHandle, x, y, width, height);
 }
 
 /**********************************************************
@@ -228,17 +301,17 @@ function drawImageOnCanvas(imgHandle, x, y)
  *********************************************************/
 function exportClickHandler()
 {
-  if (gadgets.length > 0)
+  for (let gadget of gadgets)
   {
-    gadgets[0].addPin("in");
+    gadget.addPin("in");  // DEV ONLY to add input pins
   }
 }
 
 function newClickHandler()
 {
-  if (gadgets.length > 0)
+  for (let gadget of gadgets)
   {
-    gadgets[0].addPin("out");
+    gadget.addPin("out");  // DEV ONLY to add output pins
   }
 }
 
@@ -255,6 +328,11 @@ function paletteClickHandler()
   {
     $(".palette-menu").css("display", "none");
   }
+
+  // DEV MODE
+  var wire1 = new Wire(300, 300, 1000, 200);
+  wires.push(wire1);
+  refreshCanvas();
 }
 
 /**********************************************************
@@ -263,6 +341,12 @@ function paletteClickHandler()
 function refreshCanvas()
 {
   context.clearRect(0, 0, canvas.width(), canvas.height());
+
+  // Draw wires first so that the pin images are displayed on top of the wires
+  for (let wire of wires)
+  {
+    wire.drawWire();
+  }
   for (let gadget of gadgets)
   {
     gadget.drawGadgetOnCanvas();
@@ -274,62 +358,60 @@ function refreshCanvas()
  *********************************************************/
 function canvasClickBegin(e)
 {
-  x = e.offsetX;
-  y = e.offsetY;
+  var inputPos = getInputPositionRelativeToCanvas(e);
 
   for (let gadget of gadgets)
   {
-    //console.log("Gadget Dimensions: width = " + gadget.width + "; height = " + gadget.height);
-    if (x > gadget.x &&
-        x < gadget.x + gadget.width &&
-        y > gadget.y &&
-        y < gadget.y + gadget.height)
+    if (!isItemHeld &&
+        inputPos.x > gadget.x &&
+        inputPos.x < gadget.x + gadget.width &&
+        inputPos.y > gadget.y &&
+        inputPos.y < gadget.y + gadget.height)
     {
-      //console.log("Detected a click on a gadget.");
       gadget.isHeld = true;
+      isItemHeld = true;
+      gadget.offsetX = inputPos.x - gadget.x;
+      gadget.offsetY = inputPos.y - gadget.y;
 
-      gadget.offsetX = x - gadget.x;
-      gadget.offsetY = y - gadget.y;
+      break;  // Only grab one gadget
+    }
+    else if (!isItemHeld)  // TODO: check for pin hit on click. Create new wire.
+    {
+
     }
   }
-
+  // DEV MODE
+  if (wires.length > 0)
+  {
+    wires[0].output.x = inputPos.x;
+    wires[0].output.y = inputPos.y;
+    refreshCanvas();
+  }
   isMouseDown = true;
 }
 
 function canvasMouseMove(e)
 {
+  if ("buttons" in e)
+  {
+    const MOUSE_BUTTON_LEFT = 1;
+    if ((e.buttons & MOUSE_BUTTON_LEFT) == 0)  // Check to see if the left mouse button is not pressed
+    {
+      canvasClickEnd(e);
+    }
+  }
   if (isMouseDown)
   {
-    x = e.offsetX;
-    y = e.offsetY;
+    var inputPos = getInputPositionRelativeToCanvas(e);
 
     for (let gadget of gadgets)
     {
       if (gadget.isHeld)
       {
-        gadget.x = x - gadget.offsetX;
-        gadget.y = y - gadget.offsetY;
+        gadget.x = inputPos.x - gadget.offsetX;
+        gadget.y = inputPos.y - gadget.offsetY;
 
-        // Left bounds
-        if (gadget.x < 0)
-        {
-          gadget.x = 0;
-        }
-        // Right bounds
-        if (gadget.x + gadget.width > canvas.width())
-        {
-          gadget.x = canvas.width() - gadget.width;
-        }
-        // Top Bounds
-        if (gadget.y < 0)
-        {
-          gadget.y = 0;
-        }
-        // Bottom bounds
-        if (gadget.y + gadget.height > canvas.height())
-        {
-          gadget.y = canvas.height() - gadget.height;
-        }
+        gadget.doBoundsCheck(canvas.width(), canvas.height());
       }
     }
 
@@ -348,6 +430,17 @@ function canvasClickEnd(e)
   }
 
   isMouseDown = false;
+  isItemHeld = false;
+}
+
+function getInputPositionRelativeToCanvas(e)
+{
+  // Get mouse position relative to canvas
+  var canvasOffset = canvas.offset();
+  x = e.pageX - canvasOffset.left;
+  y = e.pageY - canvasOffset.top;
+
+  return {x, y};
 }
 
 /**********************************************************
@@ -355,21 +448,30 @@ function canvasClickEnd(e)
  *********************************************************/
 function init()
 {
+  // Initialize canvas attributes
   var canvasMarginTop = parseInt(canvas.css("margin-top"), 10);
   canvas.attr({
     width: window.innerWidth,
     height: window.innerHeight - canvasMarginTop,
   });
 
+  // Gadget creation (TEMPORARY DEV MODE!!)
   calc = new Gadget("calculator", "MyCalculator");
-  calc.baseImg.onload = $.proxy(calc.handleImageLoad, calc);
   calc.baseImg.src = "assets/icons/gadget_base.png";
 
+  var timer = new Gadget("timer", "DelayTimer");
+  timer.baseImg.src = "assets/icons/gadget_base.png";
+  timer.x = 200;
+  timer.y = 200;
+
+  // Assign input handlers
   canvas.mousedown(canvasClickBegin);
-  canvas.mousemove(canvasMouseMove);
+  $(document).mousemove(canvasMouseMove);
   canvas.mouseup(canvasClickEnd);
 
+  // Add gadget objects
   gadgets.push(calc);
+  gadgets.push(timer);
 }
 
 $(init);
